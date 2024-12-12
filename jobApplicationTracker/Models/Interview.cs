4844d7e218eebfe094c7567b7c1ed485a5bfa873
NIAGARA\darkl <ivan.andrau@gmail.com>
2024-12-12 18:50:27 +0100

    Feat: Add Status table

--- jobApplicationTracker/Models/Interview.cs
+++ jobApplicationTracker/Models/Interview.cs
@@ -7,8 +7,8 @@ namespace jobApplicationTrackerApi.Models
         [Key]
         public int Id { get; set; }                 //could be [Guid]
         public int JobApplicationId { get; set; }   //could be [Guid]
-        [MaxLength(50, ErrorMessage = "Type should be less than 50 characters")]
         [Required]
+        [MaxLength(50, ErrorMessage = "Type should be less than 50 characters")]
         public string Type { get; set; }
         public DateTime? Date { get; set; }      // I made Date nullable in case you don't know the date of interview
         public string Notes { get; set; } = string.Empty;   // should it be string.Empty or string? 
---
+++ jobApplicationTracker/Models/Status.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace jobApplicationTrackerApi.Models
+{
+    public class Status
+    {
+        [Key]
+        public int Id { get; set; }     //could be [Guid]
+        [Required]
+        [MaxLength(50, ErrorMessage = "Status name should not exceed 50 characters")]
+        public string Name { get; set; }
+        [Required]
+        public int SortOrder { get; set; }
+    }
+}
