361e4df54bf1f9ed55a362a625e1961d3356afbf
NIAGARA\darkl <ivan.andrau@gmail.com>
2024-12-12 18:42:24 +0100

    Feat: Add EmploymentType model

---
+++ jobApplicationTracker/Models/EmploymentType.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace jobApplicationTrackerApi.Models
+{
+    public class EmploymentType
+    {
+        [Key]
+        public int Id { get; set; }     //could be [Guid]
+        [Required]
+        public string Name { get; set; }
+    }
+}
