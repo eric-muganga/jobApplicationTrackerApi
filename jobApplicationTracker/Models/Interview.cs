166c9773e123f38651d95cb0eaaf42fef873ca05
NIAGARA\darkl <ivan.andrau@gmail.com>
2024-12-12 18:37:07 +0100

    Feat: Add Interview table

---
+++ jobApplicationTracker/Models/Interview.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace jobApplicationTrackerApi.Models
+{
+    public class Interview
+    {
+        [Key]
+        public int Id { get; set; }                 //could be [Guid]
+        public int JobApplicationId { get; set; }   //could be [Guid]
+        [MaxLength(50, ErrorMessage = "Type should be less than 50 characters")]
+        [Required]
+        public string Type { get; set; }
+        public DateTime? Date { get; set; }      // I made Date nullable in case you don't know the date of interview
+        public string Notes { get; set; } = string.Empty;   // should it be string.Empty or string? 
+    }
+}
