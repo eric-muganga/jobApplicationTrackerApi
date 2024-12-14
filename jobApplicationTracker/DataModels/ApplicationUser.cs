62e2c601dc49126107bfacca780d52ff4906b603
Eric Muganga <105128548+eric-muganga@users.noreply.github.com>
2024-12-15 00:41:12 +0100

    feat(Datamodels): add ApplicationUser class extending IdentityUser<Guid> with custom properties and navigation

---
+++ jobApplicationTracker/DataModels/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace jobApplicationTrackerApi.DataModels;
+
+public class ApplicationUser : IdentityUser<Guid>
+{
+    public string FirstName { get; set; }
+    
+    public string FullName { get; set; }
+    
+    public ICollection<JobApplication> JobApplications { get; set; }
+}
