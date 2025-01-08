214e1e6d5159607c88768b6802745a600a76f063
Eric Muganga <105128548+eric-muganga@users.noreply.github.com>
2025-01-08 20:55:48 +0100

    feat(viewmodel): Include StatusId and Status string in JobApplicationView

--- jobApplicationTracker/ViewModels/JobApplicationView.cs
+++ jobApplicationTracker/ViewModels/JobApplicationView.cs
@@ -2,17 +2,18 @@
 {
     public class JobApplicationView
     {
-        public Guid Id { get; set; }
+        public Guid? Id { get; set; }
         public string Company { get; set; }
         public string JobTitle { get; set; }
         public Guid StatusId { get; set; }
+        public string? Status { get; set; }
         public DateTime? ApplicationDate { get; set; }
         public DateTime? InterviewDate { get; set; }
         public string Notes { get; set; }
         public Guid ContractTypeId { get; set; }
         public string? JobDescription { get; set; }
         public DateTime CreatedAt { get; set; }
-        public Guid FinancialInformationId { get; set; }    //public FinancialInformationViewModel FinancialInformation { get; set; }
+        public FinancialInformationView? FinancialInformation { get; set; } // Include FinancialInformation
 
     }
 }
