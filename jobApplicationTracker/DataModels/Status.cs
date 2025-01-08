74755c315d2629346360d70e3e884511f55af3a7
Eric Muganga <105128548+eric-muganga@users.noreply.github.com>
2025-01-08 21:35:31 +0100

    refactor(status): comment out SortOrder property

--- jobApplicationTracker/DataModels/Status.cs
+++ jobApplicationTracker/DataModels/Status.cs
@@ -5,7 +5,7 @@ namespace jobApplicationTrackerApi.DataModels
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
-        public int SortOrder { get; set; }
+        //public int SortOrder { get; set; }
 
 
         public ICollection<JobApplication> JobApplications { get; set; }
